a3120e72fb2b99beb068691cd66ae706d23a9e9e
Joe Straitiff <jstraitiff@gmail.com>
2014-02-11 22:38:48 -0800

    testing a bit more of input_file

--- test/models/import_file_test.rb
+++ test/models/import_file_test.rb
@@ -11,6 +11,7 @@ class ImportFileTest < ActiveSupport::TestCase
     imp = ImportFile.new(file: file)
     
     assert imp.valid?
+    assert_equal 'example_input.tab', imp.filename
     
     assert_difference(['Customer.count', 'Merchant.count', 'Item.count'], 3) do
       assert_difference('Purchase.count', 4) do
@@ -22,6 +23,8 @@ class ImportFileTest < ActiveSupport::TestCase
     assert_equal 1, Customer.where(name: "Snake Plissken").count
     assert_equal 1, Item.where(description: "$20 Sneakers for $5").count
     assert_equal 1, Merchant.where(name: "Sneaker Store Emporium").count
+
+    expected_results(imp.results, processed_rows: 4, revenue: 95.0, successful_rows: 4, error_rows: [])
   end
 
   test "import invalid file" do
@@ -29,12 +32,21 @@ class ImportFileTest < ActiveSupport::TestCase
     imp = ImportFile.new(file: file)
     
     assert imp.valid?
+    assert_equal 'bad_input.tab', imp.filename
     
     assert_difference(['Customer.count', 'Merchant.count', 'Item.count', 'Purchase.count'], 0) do
       assert imp.import
     end
     
-    
+    results = imp.results
+    expected_results(results, processed_rows: 6, revenue: 0, successful_rows: 0)
+    assert_equal 6, results[:error_rows].size
+  end
+  
+  def expected_results(results, expected)
+    expected.keys.each do |k|
+      assert_equal expected[k], results[k]
+    end
   end
   
 end
